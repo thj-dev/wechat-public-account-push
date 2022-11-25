1a35fa83b56b4872b97c8a22522407198a6c9fb7
thj-dev <58326901+thj-dev@users.noreply.github.com>
2022-11-25 09:30:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ export const config = {
       // 想要发送的人的名字
       name: "乖乖",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "o1Y",
+      id: "o1t7d6ugr9FXtkvlvC4UurgWhhZY",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "6Uldg9I",
       // 所在省份
